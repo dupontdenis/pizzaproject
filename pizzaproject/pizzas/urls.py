7f979750bab4e7db2f2433243d96cb2a82984efa
denis dupont <denis.dupont@univ-evry.fr>
2026-09-11 08:50:25 +0200

    add filter route

--- pizzaproject/pizzas/urls.py
+++ pizzaproject/pizzas/urls.py
@@ -1,7 +1,18 @@
 from django.urls import path
-from .views import PizzaListApi, PizzaDetailByNameApi
+from .views import (
+    PizzaListApi,
+    PizzaDetailByNameApi,
+    PizzaByIngredientApi,
+    PizzaByIngredientsApi
+)
+
+print(">>> pizzas.urls LOADED")  # doit être AVANT urlpatterns
 
 urlpatterns = [
     path('pizzas/', PizzaListApi.as_view()),
     path('pizzas/<str:name>/', PizzaDetailByNameApi.as_view()),
+    path('pizzas/ingredient/<str:ingredient>/', PizzaByIngredientApi.as_view()),
+    path('pizzas/filter/', PizzaByIngredientsApi.as_view()),
 ]
+
+
--- pizzaproject/pizzas/views.py
+++ pizzaproject/pizzas/views.py
@@ -20,3 +20,31 @@ class PizzaDetailByNameApi(APIView):
         serializer = PizzaSerializer(pizza)
         return Response(serializer.data)
 
+# class PizzaByIngredientApi(APIView):
+#     def get(self, request, ingredient):
+#         pizzas = Pizza.objects.filter(ingredients__contains=[ingredient])
+#         serializer = PizzaSerializer(pizzas, many=True)
+#         return Response(serializer.data)
+
+class PizzaByIngredientApi(APIView):
+    def get(self, request, ingredient):
+        pizzas = Pizza.objects.all()
+        pizzas = [p for p in pizzas if ingredient in p.ingredients]
+        serializer = PizzaSerializer(pizzas, many=True)
+        return Response(serializer.data)
+
+class PizzaByIngredientsApi(APIView):
+    print(">>> PizzaByIngredientsApi LOADED")
+    def get(self, request):
+        raw = request.GET.get("ingredients")
+        pizzas = Pizza.objects.all()
+
+        if raw:
+            ingredients = raw.split(",")
+            pizzas = [
+                p for p in pizzas
+                if all(ing in p.ingredients for ing in ingredients)
+            ]
+
+        serializer = PizzaSerializer(pizzas, many=True)
+        return Response(serializer.data)   
